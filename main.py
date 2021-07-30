a1e1c731ef9b012900d3d59f8840dd38460cc832
rinAkhm <rin_akhm@bk.ru>
2021-07-30 09:27:08 +0300

    before push in github

--- main.py
+++ main.py
@@ -1,28 +1,45 @@
 from errors import InputParameterVerificationError, ParamVerificationIsZero, ResultVerificationError
-from jsonschema import validate
+import jsonschema
 import re
+import json
 import logging
-from typing import Any
+from typing import Any, Callable
+import os
 
 
 def log():
+    """Логирует ошибки."""
     log = logging.getLogger(__name__)
     return log
 
 
-def default_behaviour():
-    """Дефолтная функция"""
+def default_state(param):
+    """Просто дефолтная функция."""
+    log().error('Попытки запуска исчерпаны')
     return 'Счётчик сработал'
 
 
 def result_validation(user_account: dict, json_schema: Any):
-    if isinstance(user_account, dict):
-        validate(user_account, json_schema)
-        return True
+    """Проверяет вилидацию json."""
+    if isinstance(json_schema, dict):
+        try:
+            jsonschema.validate(user_account, json_schema)
+            return user_account
+        except (jsonschema.SchemaError, jsonschema.FormatError):
+            log().error('Ошибка валидации json')
+            return dict()
+    else:
+        with open(json_schema, 'r') as file:
+            schema = json.load(file)
+            try:
+                jsonschema.validate(user_account, schema)
+                return user_account
+            except (jsonschema.SchemaError, jsonschema.FormatError) as err:
+                log().error(err)
+                return dict()
 
 
-
-def input_valid_data(email: str, password: str) -> Any:
+def input_valid_data(email: str, password: str) -> [str, str]:
     """Проверка на заполнение корректного email"""
     try:
         is_valid_email = re.compile(r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)+$').search(email)[0]
@@ -34,37 +51,50 @@ def input_valid_data(email: str, password: str) -> Any:
         return is_valid_email, is_valid_password
 
 
-def valid_all(input_valid_line, on_fail_repeat_times):
+def valid_all(input_valid_data, result_validation, on_fail_repeat_times, default_behaviour: [Callable, None] = None):
+    """Свой декаратор для проверки входных параметров, а также выходного результата"""
     def decorator(func):
         def wrapper(*args, **kwargs):
-            input_email, input_password = input_valid_line(*args, **kwargs)
-            if bool(input_email) and bool(input_password) is False:
+            input_email, input_password = input_valid_data(*args)
+            if bool(input_email) and bool(input_password) is True:
                 if on_fail_repeat_times == 0:
                     raise ParamVerificationIsZero('Передан параметр on_fail_repeat_times равный 0')
                 elif on_fail_repeat_times >= 1:
-                    raise InputParameterVerificationError("Некорректный input_param")
-                elif on_fail_repeat_times < 0:
+                    param, path = func(*args, **kwargs)
+                    result_valid = result_validation(param, path)
+                    if result_valid:
+                        print('Валидация прошла успешна')
+                        return result_valid
+                    else:
+                        for i in range(1, on_fail_repeat_times + 1):
+                            result_valid = result_validation(param, path)
+                            if not result_valid:
+                                if (default_behaviour is None) and (i == on_fail_repeat_times):
+                                    raise ResultVerificationError('Некорректный result_valid')
+                                elif (bool(default_behaviour)) and (i == on_fail_repeat_times):
+                                    default_state(default_behaviour)
+                                    break
+                else:
                     while True:
-                        input_email, input_password = input_valid_line(*args, **kwargs)
+                        input_email, input_password = input_valid_data(*args, **kwargs)
                         if bool(input_email) and bool(input_password):
                             return True
             else:
-                print(input_email, input_password)
-                rez = func(*args, **kwargs)
-                return rez
+                raise InputParameterVerificationError("Некорректный input_param")
 
         return wrapper
 
     return decorator
 
 
-@valid_all(input_valid_data, on_fail_repeat_times=1, default_behaviour=None)
+@valid_all(input_valid_data, result_validation, on_fail_repeat_times=3, default_behaviour='не ноне')
 def valid_data(email, password):
     """Формирует учетную запись в виде словаря из переданных данных."""
     user_account = dict()
     user_account['email'] = email
     user_account['password'] = password
-    return user_account
+    path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'jsonschema.json')
+    return user_account, path
 
 
-print(valid_data('rin_akhm@bk.ru', ''))
+print(valid_data('rin_akhm@bk.ru', 'pasSword2'))
